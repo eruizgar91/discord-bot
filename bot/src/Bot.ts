03d3404e7db85bcc92b32dd3d3f14affd2e9ffbe
enrique <enrique@keyko.io>
2022-08-05 20:17:24 +0200

    Some changes

--- bot/src/Bot.ts
+++ bot/src/Bot.ts
@@ -1,18 +1,19 @@
 import { Client, ClientOptions } from 'discord.js'
 import interactionCreate from './listeners/interactionCreate'
 import ready from './listeners/ready'
+;(async () => {
+  const token =
+    'MTAwNTA1NzkwMjI0OTI2NzIwMA.G-MSVY.hVgF_KcCAxGx-BuNnX56_GPIfnGOYhRpDj6AWs'
+  console.log('Bot is starting...')
 
-const token =
-  ''
+  const client = new Client({
+    intents: [],
+  })
 
-console.log('Bot is starting...')
+  ready(client)
 
-const client = new Client({
-  intents: [],
-})
+  client.login(token)
+  interactionCreate(client)
 
-ready(client)
-client.login(token)
-interactionCreate(client)
-
-console.log(client)
+  console.log('client is ready')
+})()
--- bot/src/commands/Hello.ts
+++ bot/src/commands/Hello.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction, Client } from 'discord.js'
 import { Command } from '../Command'
+import { ethers } from 'ethers'
 
 export const Hello: Command = {
   name: 'hello',
@@ -9,7 +10,10 @@ export const Hello: Command = {
     client.users.fetch(interaction.user).then((user) => {
       try {
         //Send the message to the user to sign 
-        user.send(`Can you sign the message? http://localhost:3000`)
+        const token = ethers.utils.hexValue(ethers.utils.randomBytes(20))
+
+        user.send(`Can you sign the message? http://localhost:3000?${token}`)
+        
       } catch (err) {
         console.log('err')
       }
